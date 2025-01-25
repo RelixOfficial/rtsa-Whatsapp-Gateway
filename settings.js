dd858283b1538cb622336ca52c7b30de5e48860d
RelixOfficial <relixofficial4@gmail.com>
2025-01-25 19:56:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ global.countrycode = "ID"; // Country Code - https://countrycode.org/ (ISO CODES
 global.countrycodephone = "62"; // Country Phone - https://countrycode.org/ (COUNTRY CODE)
 global.timezone = "Asia/Jakarta"; // Time Zone
 global.usePairingNumber = false; // true = Pairing Code / false = QRCode
-global.pairingNumber = ""; // whatsapp number used as a bot, for pairing number
+global.pairingNumber = "6285877276864"; // whatsapp number used as a bot, for pairing number
 //========================================================
 
 global.pp_bot = fs.readFileSync("./image/logo.png"); // location and name of the logo
@@ -14,7 +14,7 @@ global.use_pp = true; // use a logo?
 
 //========================================================
 
-global.kontakadmin = ["6281287123512"]; // admin whatsapp number
+global.kontakadmin = ["6285877276864"]; // admin whatsapp number
 global.kirimkontak_admin = false; // true = automatically send admin contact
 
 //========================================================
